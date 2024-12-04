bfabba01bf36137be52a9df7d2e8cd05bf649f0a
staffan-haglund <stahag@gmail.com>
2024-12-04 20:05:05 +0100

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MAP_DIR = os.path.join(BASE_DIR, "data", "maps")
-print("The  BASE_DIR is:", BASE_DIR)
-print("The  MAP_DIR is:", MAP_DIR)
