08f432fa7b42df33ee438db71a374980f233ec56
staffan-haglund <stahag@gmail.com>
2024-12-04 20:02:12 +0100

    LLR-8 Läser in en kartfil, kollar om den saknas eller är tom, samt om det finns andra tecken än dom tillåtna

---
+++ src/simulation/map.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+MAP_DIR = os.path.join(BASE_DIR, "data", "maps/")
+
+class Map:
+    def __init__(self, file_name = "underground.txt") -> None:
+        self._file_name = file_name
+        self._map = self.read_map_file(file_name)
+
+
+    def print_map(self):
+        for row in self._map:
+            print(f'{row}')
+
+
+    def convert_digits(self, value):
+        if value.isdigit():  # Check if the value is a digit
+            return int(value)  # Convert to integer
+        return value  # Return the original value if it's not a digit
+
+
+    def valid_map(self, map):
+        for row in map:
+            for col in row:
+                if col not in ['x', 'B', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+                    print(col)
+                    return False
+        return True
+
+
+    def read_map_file(self, file: str):
+        MAP_FILE = os.path.join(MAP_DIR, file)
+        map = [[]]
+
+        if not os.path.exists(MAP_FILE):
+            print("File not found")
+            return map
+        if os.path.getsize(MAP_FILE) == 0:
+            print("File is empty")
+            return map
+        
+        try:
+            with open(MAP_FILE, newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    converted_row = [self.convert_digits(item) for item in row]
+                    map.append(converted_row)
+            if self.valid_map(map):
+                return map
+            else:
+                return []
+
+        except Exception as e:
+            print(f'{e}')
+            return []
+
+
+new_map = Map(file_name='undergroundempty.txt')
+new_map.print_map()
---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MAP_DIR = os.path.join(BASE_DIR, "data", "maps")
+print("The  BASE_DIR is:", BASE_DIR)
+print("The  MAP_DIR is:", MAP_DIR)
