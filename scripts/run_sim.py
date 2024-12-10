dee6a9592759cca7ed2bfef7116d762e05c01a8a
CarlD <cada1313@gmail.com>
2024-12-10 09:22:53 +0100

    run_sim added

---
+++ scripts/run_sim.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from simulation.map import Map
+from simulation.get_fleet import get_fleet
+
+
+def run_sim(fleet_name:str, map_name:str):
+    sim_map = Map(map_name)._map
+    for line in sim_map:
+        if not len(line):
+            sim_map.remove(line)
+    sub_list = get_fleet(fleet_name, sim_map)
+    for sub in sub_list:
+        sub.get_new_route()
+        print(sub.planned_route)
+    
+if __name__ == "__main__":
+    run_sim(fleet_name="simple.txt", map_name="underground.txt")
---
+++ src/simulation/get_fleet.py
@@ -0,0 +1,48 @@
+import os
+from simulation.submarine import Submarine
+
+
+def get_fleet(fleet_name: str, map: list) -> str:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    SUB_DIR = os.path.join(BASE_DIR, "data", "fleets/")
+    SUB_FILE = os.path.join(SUB_DIR, fleet_name)
+    sub_list = []
+    with open(SUB_FILE, "r") as fleet:
+        counter = 0
+        for line in fleet.readlines():
+            if line[0].isnumeric():
+                try:
+                    temp_x0 = int(line.split(",")[0].replace(" ", ""))
+                except IndexError:
+                    temp_x0 = 1
+                try:
+                    temp_y0 = int(line.split(",")[1].replace(" ", ""))
+                except IndexError:
+                    temp_y0 = 1
+                try:
+                    temp_xe = int(line.split(",")[2].replace(" ", ""))
+                except:
+                    temp_xe = 1
+                try:
+                    temp_ye = int(line.split(",")[3].replace(" ", ""))
+                except:
+                    temp_ye = 1
+                try:
+                    temp_missiles = int(line.split(",")[4].replace(" ", ""))
+                except:
+                    temp_missiles = 1
+                sub_list.append(
+                    Submarine(
+                        id = counter,
+                        x0=temp_x0,
+                        y0=temp_y0,
+                        xe=temp_xe,
+                        ye=temp_ye,
+                        m_count=temp_missiles,
+                        map=map,
+                        map_height=len(map),
+                        map_width=len(map[0])
+                    )
+                )
+                counter += 1
+    return sub_list
